81fec00e470cf6d7eb89c5e3bbb6d098b631b318
André Miranda <andrelmmiranda@gmail.com>
2024-04-05 13:29:03 -0300

    fix: corrigindo server.js e package.json

--- server.js
+++ server.js
@@ -4,11 +4,13 @@ const router = jsonServer.router('db.json');
 
 const middlewares = jsonServer.defaults();
 
-server.use(middlewares, jsonServer.rewriter({
-    '/*': '/$1'
-}), router);
+server.use(middlewares)
+server.use(jsonServer.rewriter({
+    "/*": "/$1"
+}));
+server.use(router);
 
-server.listen(3001, ()=>{
+server.listen(3000, ()=>{
     console.log('Json Server running...')
 });
 
